dc976f347a84a541bb3b7884d901430da4de7ade
ALSHARIF <mohamed@MacBook-Pro-de-Mohamed.local>
2025-06-25 05:55:55 +0200

    Initial commit for dalel Flutter app

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Dalel());
+}
+
+class Dalel extends StatelessWidget {
+  const Dalel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+  
